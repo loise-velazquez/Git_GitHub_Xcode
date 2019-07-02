e5d2cf2c2fd915b27e50cc862573cb361cefad1b
Loise Velazquez <loise.v2194@gmail.com>
2019-07-02 10:40:49 -0700

    new file

---
+++ GettingStartedFollowAlong/gitTest.swift
@@ -0,0 +1,9 @@
+//
+//  gitTest.swift
+//  GettingStartedFollowAlong
+//
+//  Created by Loise Velazquez on 7/2/19.
+//  Copyright © 2019 Michael Panitz. All rights reserved.
+//
+
+import Foundation
